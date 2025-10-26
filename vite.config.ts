d11cde0616e3c7f4723c6df6379ce7f94d088c6b
todor534 <t.todorov534@gmail.com>
2025-10-26 16:41:31 +0200

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,3 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      scopeBehaviour: 'local',
-    },
-  },
-  server: {
-    open: true,
-  },
-});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({ plugins: [react()], base: "/", build: { sourcemap: true } });
